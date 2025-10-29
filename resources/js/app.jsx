73be0d0735dc7c4b01d05380a94782c942ad608b
SHMEBUU <nalvarado@ing.ucsc.cl>
2025-10-29 01:10:10 -0300

    Modifique App.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,14 +1,19 @@
+import './bootstrap';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
-import './bootstrap';
 
-const rootElement = document.getElementById('app');
-if (rootElement) {
-    const root = createRoot(rootElement);
-    root.render(
-        <StrictMode>
-            <App />
-        </StrictMode>
-    );
-}
+// Asegurarse que el DOM esté cargado
+document.addEventListener('DOMContentLoaded', () => {
+    const rootElement = document.getElementById('app');
+    if (rootElement) {
+        const root = createRoot(rootElement);
+        root.render(
+            <StrictMode>
+                <App />
+            </StrictMode>
+        );
+    } else {
+        console.error('No se encontró el elemento #app');
+    }
+});
